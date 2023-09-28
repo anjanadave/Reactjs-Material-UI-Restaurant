4fb6f98b8c1d6db407496613760c768e7d08ed9e
anjanadave <anju.aksh72@gmail.com>
2023-09-28 23:31:24 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,110 @@
+import React, {useState} from 'react';
+import { AppBar, Box, Divider, Drawer, IconButton, Toolbar, Typography } from '@mui/material';
+import Logo from "../../images/logo.svg";
+import MenuIcon from '@mui/icons-material/Menu';
+import {NavLink} from 'react-router-dom';
+import '../../styles/HeaderStyles.css';
+
+const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false)
+  //  handle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+  // menu drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{textAlign:'center'}}>
+       <Typography
+              color={'goldenrod'}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, my: 2 }}
+              >
+             <img src={Logo} alt="logo" height={"70px"} width="250px" />
+              </Typography>
+                <Box>
+               <ul className="mobile-navigation">
+                <li>
+                  <NavLink activeClassName="active" to={'/'}>Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/menu'}>Menu</NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/about'}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/contact'}>Contact</NavLink>
+                </li>
+               </ul>
+              </Box>
+    </Box>
+  )
+  return (
+    <div>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <IconButton 
+            color="inherit" 
+            aria-lable="open drawer"
+             edge="start" 
+             sx={{
+              mr:2,
+              display:{sm: "none"}, 
+              }}
+              onClick={handleDrawerToggle}
+              >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={'goldenrod'}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}>
+              <img src={Logo} alt="logo" height={"70px"} width="200px" />
+
+              </Typography>
+              <Divider />
+              <Box sx={{display:{xs:'none', sm:"block"}}}>
+               <ul className="navigation-menu">
+                <li>
+                  <NavLink activeClassName="active" to={'/'}>Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/menu'}>Menu</NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/about'}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/contact'}>Contact</NavLink>
+                </li>
+               </ul>
+              </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+           varient="temporary"
+            open={mobileOpen} 
+            onClose={handleDrawerToggle}
+            sx={{display:{xs:'block', sm:"none"}, 
+            "& .MuiDrawer-paper" : {
+              boxSizing:'border-box',
+              width:"240px",
+            },
+          }}
+            >
+           {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+        <Toolbar />
+        </Box>
+      </Box>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/data/Data.js
@@ -0,0 +1,50 @@
+import Dosa from '../images/dosa.jpg'
+import Chola from '../images/chhola.jpg'
+import Idli from '../images/idli.jpg'
+import MasalaDosa from '../images/masala.jpg'
+import Paneer from '../images/paneer.jpg'
+import Gujarati from '../images/gujrati.jpeg'
+
+export const MenuList = [
+    {
+        name:'Dosa',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.',
+        image:Dosa,
+        price:200
+    },
+
+    {
+        name:'Chola',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.',
+        image:Chola,
+        price:250
+    },
+
+    {
+        name:'Idli Sambhar',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.',
+        image:Idli,
+        price:300
+    },
+
+    {
+        name:'Masala Dosa',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.',
+        image:MasalaDosa,
+        price:100
+    },
+
+    {
+        name:'Paneer',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.',
+        image:Paneer,
+        price:200
+    },
+
+    {
+        name:'Gujarati',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.',
+        image:Gujarati,
+        price:500,
+    },
+]
